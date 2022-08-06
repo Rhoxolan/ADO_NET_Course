9450789cc96b8edfa18283d35a62eb152c43ac9e
Rhoxolan <pba17mail@gmail.com>
2022-08-06 22:35:53 +0300

    Add

--- 2022.07.04_PW/Program.cs
+++ 2022.07.04_PW/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            string connStr = "Data Source =(localdb)\\ MSSQLLocalDB; Initial Catalog=StudentsGrades; Integrated Security=SSPI;";
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine($"Server Name: {conn.DataSource}");
+                    Console.WriteLine($"DataBase Name: {conn.Database}");
+                    Console.WriteLine($"ServerVersion: {conn.ServerVersion}");
+                    Console.WriteLine($"Connection State: {conn.State}");
+                    Console.WriteLine("\nПодключение было выполнено успешно!");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    conn?.Close();
+                }
+            }
+        }
+    }
+}
