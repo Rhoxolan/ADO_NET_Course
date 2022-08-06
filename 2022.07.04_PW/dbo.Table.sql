e6b2dd317f30047ca43b0b833668f774c1f76e9f
pba17 <pba17@PAVEL>
2022-08-06 21:38:40 +0300

    Добавьте файлы проекта.

---
+++ 2022.07.04_PW/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StudentsGrades]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FullName] NVARCHAR(100) NOT NULL CHECK(FullName != ''), 
+    [GroupName] NVARCHAR(30) NOT NULL DEFAULT 'Приёмная группа', 
+    [AverageGradesForYears] FLOAT NULL, 
+    [NameOfMinAVGGradesSubject] NVARCHAR(30) NULL, 
+    [NameOfMaxAVGGradesSubject] NVARCHAR(30) NULL,
+)
