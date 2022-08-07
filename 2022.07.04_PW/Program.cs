765401a624f82787ef45b494345bdda98533114d
Rhoxolan <pba17mail@gmail.com>
2022-08-07 10:28:11 +0300

    Add

--- 2022.07.04_PW/Program.cs
+++ 2022.07.04_PW/Program.cs
@@ -20,7 +20,7 @@ namespace MyProgram
                     Console.WriteLine($"Connection State: {conn.State}");
                     Console.WriteLine("\nПодключение было выполнено успешно!");
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                     conn?.Close();
@@ -35,7 +35,23 @@ namespace MyProgram
                 try
                 {
                     conn.Open();
-                    SqlCommand cmd = new("SELECT * FROM StudentsGrades", conn); //Разобраться, как правильно выводить всё. Посмотреть презентацию, методичку, метанит и пример
+                    SqlCommand cmd = new();
+                    cmd.Connection = conn;
+                    //cmd.CommandText = "INSERT StudentsGrades VALUES " +  //Добавляем строки в таблицу
+                    //    "(N'Петров Петр Петрович', N'Группа 5', 10, N'История', N'География'), " +
+                    //    "(N'Максименко Максим Максимович', N'Группа 7', 9, N'Английский', N'Математика'), " +
+                    //    "(N'Алексеенко Алексей Алексеевич', N'Группа 3', 5, N'История', N'Математика')";
+                    //cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "SELECT * FROM StudentsGrades"; //Выбираем все значения
+                    SqlDataReader sqlDataReader = cmd.ExecuteReader();
+                    Console.WriteLine($"{sqlDataReader.GetName(0)}\t{sqlDataReader.GetName(1)}\t{sqlDataReader.GetName(2)}\t{sqlDataReader.GetName(3)}\t{sqlDataReader.GetName(4)}");
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine($"{sqlDataReader.GetValue(0)}\t{sqlDataReader.GetValue(1)}\t{sqlDataReader.GetValue(2)}\t{sqlDataReader.GetValue(3)}\t{sqlDataReader.GetValue(4)}");
+                    }
+                    //Ты тут. Разобраться с красивым выводом.
+                    sqlDataReader.Close();
                 }
                 catch (Exception ex)
                 {
