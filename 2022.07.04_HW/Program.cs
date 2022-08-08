ef3c85acd0d193b687ba91db155b1958677b1b43
Rhoxolan <pba17mail@gmail.com>
2022-08-08 22:09:36 +0300

    Add

--- 2022.07.04_HW/Program.cs
+++ 2022.07.04_HW/Program.cs
@@ -88,11 +88,68 @@ namespace _2022._07._04_HW
 
                     //Выполнение нескольких запросов сразу через NextResult() - Добавить в примечание
                     //Показать максимальную, минимальную и среднюю калорийность;
-                    cmd.CommandText = "";
+                    cmd.CommandText = "SELECT DISTINCT Calories FROM FruitsAndVegetables WHERE Calories = (SELECT MAX(Calories) FROM FruitsAndVegetables); " +
+                        "SELECT DISTINCT Calories FROM FruitsAndVegetables WHERE Calories = (SELECT MIN(Calories) FROM FruitsAndVegetables); " +
+                        "SELECT DISTINCT Calories FROM FruitsAndVegetables WHERE Calories BETWEEN ((SELECT AVG(Calories) FROM FruitsAndVegetables) - 30) AND ((SELECT AVG(Calories) FROM FruitsAndVegetables) + 30);";
+                    sqlDataReader = cmd.ExecuteReader();
+                    int line2 = 0;
+                    do
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            if (line2 == 0)
+                            {
+                                for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                                {
+                                    Console.Write($"{sqlDataReader.GetName(i),15}");
+                                }
+                                Console.WriteLine();
+                            }
+                            Console.WriteLine($"{sqlDataReader[0],15}");
+                            line2++;
+                        }
+                    } while (sqlDataReader.NextResult());
+                    Console.WriteLine("\n");
+                    sqlDataReader.Close();
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine("\n\n\n");
+
+            //Задание 4
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new();
+                    cmd.Connection = conn;
+
+                    //Показать количество овощей;
+                    cmd.CommandText = "SELECT COUNT(FruitsAndVegetables.Id) FROM FruitsAndVegetables WHERE FruitsAndVegetables.Type = 2";
+                    object countVeg = cmd.ExecuteScalar();
+                    Console.WriteLine($"Количество овощей: {countVeg}");
+                    Console.WriteLine("\n");
+
+                    //Показать количество фруктов
+                    cmd.CommandText = "SELECT COUNT(FruitsAndVegetables.Id) FROM FruitsAndVegetables WHERE FruitsAndVegetables.Type = 1";
+                    object countFruit = cmd.ExecuteScalar();
+                    Console.WriteLine($"Количество овощей: {countFruit}");
+                    Console.WriteLine("\n");
+
+                    //Показать количество овощей и фруктов заданного цвета;
+                    cmd.CommandText = "SELECT COUNT(FruitsAndVegetables.Id) FROM FruitsAndVegetables WHERE FruitsAndVegetables.Color = N'Зелёный'";
+                    object countFruitNVegAtColor = cmd.ExecuteScalar();
+                    Console.WriteLine($"Количество овощей и фруктов заданного цвета: {countFruitNVegAtColor}");
+                    Console.WriteLine("\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    conn?.Close();
                 }
             }
             Console.WriteLine("\n\n\n");
