41cc5e7bb2fe92edfc8f525090a06d8a6e12786c
Rhoxolan <pba17mail@gmail.com>
2022-08-08 18:46:27 +0300

    Add Home Work

---
+++ 2022.07.04_HW/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FruitsAndVegetables]
+(
+	[Id] INT NOT NULL IDENTITY PRIMARY KEY,
+	[Name] NVARCHAR(100) NOT NULL CHECK(Name != ''),
+	[Type] BIT NOT NULL,
+	[Color] NVARCHAR(30) NOT NULL CHECK(Color != ''),
+	[Calories] INT NULL
+)
