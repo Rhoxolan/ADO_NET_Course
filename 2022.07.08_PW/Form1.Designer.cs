93157c6c76fe71f7b857f8da747ac523c3f3f73a
Rhoxolan <pba17mail@gmail.com>
2022-08-14 14:51:05 +0300

    Add to PW

--- 2022.07.08_PW/Form1.Designer.cs
+++ 2022.07.08_PW/Form1.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверить подключение";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(750, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(12, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(372, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Отобразить всю информацию о концтоварах";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(390, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(372, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Отображение всех типов канцтоваров";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(12, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(372, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Отображение всех менеджеров по продажам";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(390, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(372, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Показать канцтовары с максимальным количеством единиц";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(12, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(372, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Показать канцтовары с минимальным количеством единиц";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button7.Location = new System.Drawing.Point(390, 255);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(372, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Показать канцтовары с минимальным количеством единиц";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button8.Location = new System.Drawing.Point(12, 284);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(372, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Показать канцтовары с минимальной себестоимостью единицы";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button9.Location = new System.Drawing.Point(390, 284);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(372, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Показать канцтовары с максимальной себестоимостью единицы";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(776, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
     }
 }
--- 2022.07.08_PW/Form1.cs
+++ 2022.07.08_PW/Form1.cs
@@ -1,10 +1,40 @@
+using System.Configuration;
+using System.Data.SqlClient;
+
 namespace _2022._07._08_PW
 {
     public partial class Form1 : Form
     {
+        string connStr;
+
         public Form1()
         {
+            connStr = ConfigurationManager.ConnectionStrings["mySqlConn"].ConnectionString;
             InitializeComponent();
         }
+
+        //Задание 2. Проверка подключения
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    MessageBox.Show("Подключение было выполнено успешно!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    conn?.Close();
+                }
+                finally
+                {
+                    conn?.Close();
+                }
+            }
+        }
+
+
     }
 }
