8ed7c1dbb2e1581ca2157aa3929660ddd1b59369
Rhoxolan <pba17mail@gmail.com>
2022-08-17 20:13:25 +0300

    Add to HW

--- 2022.07.13_HW/Form1.Designer.cs
+++ 2022.07.13_HW/Form1.Designer.cs
@@ -28,12 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 188);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(56, 206);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(278, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "СУБД";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вывести всё";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(94, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Color";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(136, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Calories";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 279);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 19);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(57, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(31, 19);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(94, 279);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 19);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(136, 279);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 19);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Добавить в БД";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button2;
     }
 }
--- 2022.07.13_HW/Form1.cs
+++ 2022.07.13_HW/Form1.cs
@@ -1,10 +1,112 @@
+using Npgsql;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
 namespace _2022._07._13_HW
 {
     public partial class Form1 : Form
     {
+        string connStr;
+        DataTable dt;
+        DbProviderFactory providerFactory;
+
         public Form1()
         {
             InitializeComponent();
+            connStr = string.Empty;
+            dt = null;
+            providerFactory = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Подгрузка двух вариантов подключений к базам данных
+            DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
+            DbProviderFactories.RegisterFactory("Npgsql", NpgsqlFactory.Instance);
+            dt = DbProviderFactories.GetFactoryClasses();
+            comboBox1.DataSource = null;
+            comboBox1.DataSource = dt;
+            comboBox1.DisplayMember = "InvariantName";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Присваиваем переменной providerFactory значение выбранной базы данных 
+            if (comboBox1.SelectedIndex != -1)
+            {
+                DataRowView selectedItem = comboBox1.SelectedItem as DataRowView;
+                string providerName = selectedItem.Row.Field<string>("InvariantName");
+                ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings.OfType<ConnectionStringSettings>().LastOrDefault(t => t.ProviderName == providerName);
+                if (settings != null)
+                {
+                    //MessageBox.Show(settings.ConnectionString);
+                    connStr = settings.ConnectionString;
+                    providerFactory = DbProviderFactories.GetFactory(providerName);
+                }
+            }
+        }
+
+        //Универсальный метод для выполнения запросов
+        private async void ExecuteQuery(string query)
+        {
+            using (DbConnection connection = providerFactory.CreateConnection())
+            {
+                connection.ConnectionString = connStr;
+                DbCommand command = providerFactory.CreateCommand();
+                command.CommandText = query;
+                command.Connection = connection;
+                DataTable dt2 = new();
+                DbDataReader reader = null;
+                try
+                {
+                    await connection?.OpenAsync();
+                    reader = await command.ExecuteReaderAsync();
+                    int line = 0;
+                    dt2.Columns.Clear();
+                    while (await reader.ReadAsync())
+                    {
+                        if (line == 0)
+                            for (int i = 0; i < reader.FieldCount; i++)
+                                dt2.Columns.Add(reader.GetName(i));
+                        DataRow row = dt2.NewRow();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            row[i] = reader[i];
+                        dt2.Rows.Add(row);
+                        line++;
+                    }
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = dt2;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    reader?.Close();
+                    connection?.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExecuteQuery("SELECT * FROM FruitsAndVegetables");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataRowView selectedItem = comboBox1.SelectedItem as DataRowView;
+            if (selectedItem.Row.Field<string>("InvariantName") == "Npgsql")
+            {
+                ExecuteQuery($"INSERT INTO FruitsAndVegetables VALUES (DEFAULT, '{textBox1.Text}', {Convert.ToInt32(textBox2.Text)}, '{textBox3.Text}', {Convert.ToInt32(textBox4.Text)})");
+            }
+            if (selectedItem.Row.Field<string>("InvariantName") == "System.Data.SqlClient")
+            {
+                ExecuteQuery($"INSERT INTO FruitsAndVegetables VALUES (N'{textBox1.Text}', {Convert.ToInt32(textBox2.Text)}, N'{textBox3.Text}', {Convert.ToInt32(textBox4.Text)})");
+            }
         }
     }
 }
