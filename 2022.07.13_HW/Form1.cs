86881b393606aed8b422d21d6c8e3b86eec9d257
Rhoxolan <pba17mail@gmail.com>
2022-08-19 19:15:28 +0300

    Add

--- 2022.07.13_HW/Form1.cs
+++ 2022.07.13_HW/Form1.cs
@@ -23,6 +23,7 @@ namespace _2022._07._13_HW
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
             //Подгрузка двух вариантов подключений к базам данных
             DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
             DbProviderFactories.RegisterFactory("Npgsql", NpgsqlFactory.Instance);
@@ -51,7 +52,7 @@ namespace _2022._07._13_HW
         }
 
         //Универсальный метод для выполнения запросов
-        private void ExecuteQuery(string query)
+        private DataTable ExecuteQuery(string query)
         {
             //MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
             //Thread.Sleep(5000);
@@ -61,31 +62,31 @@ namespace _2022._07._13_HW
                 DbCommand command = providerFactory.CreateCommand();
                 command.CommandText = query;
                 command.Connection = connection;
-                DataTable dt2 = new();
+                DataTable dataTable = new();
                 DbDataReader reader = null;
                 try
                 {
                     connection?.Open();
                     reader = command.ExecuteReader();
                     int line = 0;
-                    dt2.Columns.Clear();
+                    dataTable.Columns.Clear();
                     while (reader.Read())
                     {
                         if (line == 0)
                             for (int i = 0; i < reader.FieldCount; i++)
-                                dt2.Columns.Add(reader.GetName(i));
-                        DataRow row = dt2.NewRow();
+                                dataTable.Columns.Add(reader.GetName(i));
+                        DataRow row = dataTable.NewRow();
                         for (int i = 0; i < reader.FieldCount; i++)
                             row[i] = reader[i];
-                        dt2.Rows.Add(row);
+                        dataTable.Rows.Add(row);
                         line++;
                     }
-                    dataGridView1.DataSource = null;
-                    dataGridView1.DataSource = dt2;
+                    return dataTable;
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
+                    return null;
                 }
                 finally
                 {
@@ -95,38 +96,39 @@ namespace _2022._07._13_HW
             }
         }
 
-        //Универсальный метод для выполнения запросов, вариант с передачей объектов DbCommand и DbConnection для выполнения параметризованного запроса.
-        private void ExecuteQuery2(DbCommand command)
+        //Универсальный метод для выполнения запросов, вариант с передачей объекта DbCommand для выполнения параметризованного запроса.
+        private DataTable ExecuteQuery2(DbCommand command)
         {
+            //MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
             using (DbConnection connection = providerFactory.CreateConnection())
             {
                 connection.ConnectionString = connStr;
                 command.Connection = connection;
-                DataTable dt2 = new();
+                DataTable dataTable = new();
                 DbDataReader reader = null;
                 try
                 {
                     connection?.Open();
                     reader = command.ExecuteReader();
                     int line = 0;
-                    dt2.Columns.Clear();
+                    dataTable.Columns.Clear();
                     while (reader.Read())
                     {
                         if (line == 0)
                             for (int i = 0; i < reader.FieldCount; i++)
-                                dt2.Columns.Add(reader.GetName(i));
-                        DataRow row = dt2.NewRow();
+                                dataTable.Columns.Add(reader.GetName(i));
+                        DataRow row = dataTable.NewRow();
                         for (int i = 0; i < reader.FieldCount; i++)
                             row[i] = reader[i];
-                        dt2.Rows.Add(row);
+                        dataTable.Rows.Add(row);
                         line++;
                     }
-                    dataGridView1.DataSource = null;
-                    dataGridView1.DataSource = dt2;
+                    return dataTable;
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
+                    return null;
                 }
                 finally
                 {
@@ -138,13 +140,14 @@ namespace _2022._07._13_HW
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            await Task.Run(() => ExecuteQuery("SELECT * FROM FruitsAndVegetables")); //Запускаем выполнение асинхронного метода
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = await Task.Run(() => ExecuteQuery("SELECT * FROM FruitsAndVegetables")); //Запускаем выполнение асинхронного метода
         }
 
         private async void button2_Click(object sender, EventArgs e)
         {
             DataRowView selectedItem = comboBox1.SelectedItem as DataRowView;
-            if (selectedItem.Row.Field<string>("InvariantName") == "Npgsql")
+            if (selectedItem.Row.Field<string>("InvariantName") == "Npgsql") //Эксперементируем с передачей команды универсальному методу для выполнения запроса
             {
                 string query = $"INSERT INTO FruitsAndVegetables (Name, Type, Color, Calories) VALUES (@Name, @Type, @Color, @Calories)";
                 DbCommand dbCommand = providerFactory.CreateCommand();
@@ -154,7 +157,8 @@ namespace _2022._07._13_HW
                 command.Parameters.AddWithValue("@Color", textBox3.Text);
                 command.Parameters.AddWithValue("@Calories", Convert.ToInt32(textBox4.Text));
                 command.CommandText = query;
-                await Task.Run(() => ExecuteQuery2(command));   //Запускаем выполнение асинхронного метода
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = await Task.Run(() => ExecuteQuery2(command));   //Запускаем выполнение асинхронного метода
             }
             if (selectedItem.Row.Field<string>("InvariantName") == "System.Data.SqlClient")
             {
@@ -166,7 +170,8 @@ namespace _2022._07._13_HW
                 command.Parameters.AddWithValue("@Color", textBox3.Text);
                 command.Parameters.AddWithValue("@Calories", Convert.ToInt32(textBox4.Text));
                 command.CommandText = query;
-                await Task.Run(() => ExecuteQuery2(command));   //Запускаем выполнение асинхронного метода
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = await Task.Run(() => ExecuteQuery2(command));   //Запускаем выполнение асинхронного метода
             }
             if (selectedItem.Row.Field<string>("InvariantName") == "System.Data.SQLite")
             {
@@ -178,14 +183,16 @@ namespace _2022._07._13_HW
                 command.Parameters.AddWithValue("@Color", textBox3.Text);
                 command.Parameters.AddWithValue("@Calories", Convert.ToInt32(textBox4.Text));
                 command.CommandText = query;
-                await Task.Run(() => ExecuteQuery2(command));   //Запускаем выполнение асинхронного метода
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = await Task.Run(() => ExecuteQuery2(command));   //Запускаем выполнение асинхронного метода
             }
         }
 
         private async void button3_Click(object sender, EventArgs e)
         {
             int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
-            await Task.Run(() => ExecuteQuery($"DELETE FROM FruitsAndVegetables WHERE Id = {id}"));  //Запускаем выполнение асинхронного метода
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = await Task.Run(() => ExecuteQuery($"DELETE FROM FruitsAndVegetables WHERE Id = {id}"));  //Запускаем выполнение асинхронного метода
         }
     }
 }
