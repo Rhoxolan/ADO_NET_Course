8f416b88248f3d0870d78aaca8d98a04d793093b
Rhoxolan <pba17mail@gmail.com>
2022-11-14 11:36:19 +0200

    Add

--- 2022.07.22_PW/Form1.cs
+++ 2022.07.22_PW/Form1.cs
@@ -1,19 +1,14 @@
 using _2022._07._22_PW_ClassLibrary1;
 using _2022._07._22_PW_ClassLibrary2;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel;
 
 namespace _2022._07._22_PW
 {
     public partial class Form1 : Form
     {
-        BindingList<Game> games;
-
         public Form1()
         {
             InitializeComponent();
-            games = new();
-            listBox1.DataSource = games;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -21,9 +16,22 @@ namespace _2022._07._22_PW
             using GamesContext context = new();
             context.Database.EnsureCreated();
             context.Games.Load();
-            games = context.Games.Local.ToBindingList();
+            listBox1.DataSource = context.Games.Local.ToList();
         }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Game game = new()
+            {
+                Name = textBox1.Text,
+                Publisher = textBox2.Text,
+                Genre = (GameGenre)(int)numericUpDown1.Value
+            };
+            using GamesContext context = new();
+            context.Games.Load();
+            context.Entry(game).State = EntityState.Added; //Пример "Тонкой настройки"
+            context.SaveChanges();
+            listBox1.DataSource = context.Games.Local.ToList();
+        }
     }
 }
