7a0f47222f9602d845e1b7f2336b1a7d25bb8539
Rhoxolan <pba17mail@gmail.com>
2022-12-03 09:36:04 +0200

    Add to PW

--- 2022.08.12_PW/Form1.Designer.cs
+++ 2022.08.12_PW/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверка подключения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(249, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(539, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Отображение всех покупателей";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MailingListsPromotions";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
     }
 }
--- 2022.08.12_PW/Form1.cs
+++ 2022.08.12_PW/Form1.cs
@@ -1,10 +1,34 @@
+using System.Data.SQLite;
+
 namespace _2022._08._12_PW
 {
     public partial class Form1 : Form
     {
+        private string connStr;
+
         public Form1()
         {
             InitializeComponent();
+            connStr = "Data Source=MailingListsPromotions.db;Cache=Shared";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using SQLiteConnection sqliteConnection = new(connStr);
+            try
+            {
+                sqliteConnection.Open();
+                MessageBox.Show("Подключение было выполнено успешно!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                sqliteConnection?.Close();
+            }
+            finally
+            {
+                sqliteConnection?.Close();
+            }
         }
     }
 }
